245e07610d6c6130a4cd7b5a7331b61294d21e2c
sparsh-m <sparshmisra1@gmail.com>
2022-02-26 16:21:57 +0530

    setting up gh pages

--- src/App.js
+++ src/App.js
@@ -5,12 +5,12 @@ import TextForm from './components/TextForm';
 import React, {useState} from 'react'
 import Alert from './components/Alert';
 import About from './components/About';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Link,
-  Route
-} from 'react-router-dom';
+// import {
+//   BrowserRouter as Router,
+//   Switch,
+//   Link,
+//   Route
+// } from 'react-router-dom';
 
 function App() {
   const toggle = ()=>{
@@ -40,20 +40,20 @@ function App() {
   }
   return (
     <>
-      <Router>
+      {/* <Router> */}
       <Navbar title="TextUtils" mode={mode} toggleMode = {toggle}/>
       <Alert alert={alert}/>
       <div className="container">
-        <Switch>
+        {/* <Switch>
           <Route exact path="/about">
-            <About />
-          </Route>
-          <Route exact path="/">
+            <About /> */}
+          {/* </Route> */}
+          {/* <Route exact path="/"> */}
             <TextForm heading="Enter text to Analyze" mode={mode} changeAlert={changeAlert}/>
-          </Route>
-        </Switch>
+          {/* </Route>
+        </Switch> */}
       </div>
-      </Router>
+      {/* </Router> */}
     </>
   );
 }
